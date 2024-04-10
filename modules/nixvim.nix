5013c1ec53bdcf467b92a1597da814dda629f337
sai <saitheninja@gmail.com>
2024-04-10 17:24:08 +0200

    add more nixvim config

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -4,33 +4,35 @@
   # imports = [ nixvim.nixosModules.nixvim ];
 
   options = {
-    configNixvim.enable = lib.mkEnableOption "Adds Neovim configured with nixvim.";
+    configNixvim.enable = lib.mkEnableOption "Adds Neovim, configured with nixvim.";
   };
 
   config = lib.mkIf config.configNixvim.enable {
     programs.nixvim = {
       enable = true;
 
+      globals.mapleader = " ";
+
       opts = {
         number = true;
         relativenumber = true;
+
+        shiftwidth = 2;
       };
 
-      # shiftwidth = 2;
 
       colorschemes.gruvbox.enable = true;
 
+      plugins.lsp = {
+	      enable = true;
+	      servers = {
+		      tsserver.enable = true;
+		      lua-ls.enable = true;
+	      };
+      };
+
     # plugins = {
     #   lightline.enable = true;
-
-    #   lsp = {
-    #     enable = true;
-    #     servers = {
-    #       tsserver.enable = true;
-    #       lua-ls.enable = true;
-    #     };
-    #   };
-
     #   telescope.enable = true;
     #   treesitter.enable = true;
     # };
