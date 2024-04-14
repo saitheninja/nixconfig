5a9e9bd0a78db401b4e36c31e9685e4d1f815a7a
sai <saitheninja@gmail.com>
2024-04-14 02:56:35 +0200

    conform autosave now?

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -71,11 +71,12 @@
         conform-nvim = {
           enable = true;
           formatOnSave = ''
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              pattern = "*",
-              callback = function(args)
-                require("conform").format({ bufnr = args.buf })
-              end,
+            require("conform").setup({
+              format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_fallback = true,
+              },
             })
           '';
           formattersByFt = {
