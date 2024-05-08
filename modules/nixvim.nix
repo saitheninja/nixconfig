fe1a51d489e0d83d1192418d442a180fbbbaa1cc
sai <saitheninja@gmail.com>
2024-05-08 15:22:04 +0200

    nixvim conform keymap
    "lsp-format" was adding the "Format" commands. Disabled it.

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -129,12 +129,12 @@
 
         # formatters
         conform-nvim = {
-          enable = false;
+          enable = true;
 
-          formatOnSave = {
-            lspFallback = true;
-            timeoutMs = 500;
-          };
+          # formatOnSave = {
+          #   lspFallback = true;
+          #   timeoutMs = 500;
+          # };
 
           formattersByFt = {
             # run sequentially: css = [ ...formatters ]
@@ -266,7 +266,7 @@
             yamlls.enable = true;
           };
         };
-        lsp-format.enable = true;
+        #lsp-format.enable = true;
         #lsp-lines.enable = true; # render diagnostics using virtual lines on top of the real line of code
         lspkind.enable = true; # add pictograms for lsp completion items
 
@@ -321,6 +321,7 @@
 
         # conform
         {
+          # action = "<cmd>:lua require('conform').format({ async = true, lsp_fallback = true })<CR>";
           action = "<cmd>Format<CR>";
           key = "<leader>cf";
           mode = [
@@ -447,31 +448,45 @@
             desc = "Trouble toggle document diagnostics.";
           };
         }
-        {
-          action = "<cmd>TroubleToggle quickfix<CR>";
-          key = "<leader>xq";
-          mode = "n";
-          options = {
-            desc = "Trouble toggle quickfix list.";
-          };
-        }
-        {
-          action = "<cmd>TroubleToggle loclist<CR>";
-          key = "<leader>xl";
-          mode = "n";
-          options = {
-            desc = "Trouble toggle location list."; # location list is a window-local quickfix list
-          };
-        }
-        {
-          action = "<cmd>TroubleToggle lsp_references<CR>";
-          key = "gR";
-          mode = "n";
-          options = {
-            desc = "Trouble toggle LSP references.";
-          };
-        }
+        # {
+        #   action = "<cmd>TroubleToggle quickfix<CR>";
+        #   key = "<leader>xq";
+        #   mode = "n";
+        #   options = {
+        #     desc = "Trouble toggle quickfix list.";
+        #   };
+        # }
+        # {
+        #   action = "<cmd>TroubleToggle loclist<CR>";
+        #   key = "<leader>xl";
+        #   mode = "n";
+        #   options = {
+        #     desc = "Trouble toggle location list."; # location list is a window-local quickfix list
+        #   };
+        # }
+        # {
+        #   action = "<cmd>TroubleToggle lsp_references<CR>";
+        #   key = "gR";
+        #   mode = "n";
+        #   options = {
+        #     desc = "Trouble toggle LSP references.";
+        #   };
+        # }
       ];
+
+      extraConfigLua = ''
+        vim.api.nvim_create_user_command("Format", function(args)
+          local range = nil
+          if args.count ~= -1 then
+            local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+            range = {
+              start = { args.line1, 0 },
+              ["end"] = { args.line2, end_line:len() },
+            }
+          end
+          require("conform").format({ async = true, lsp_fallback = true, range = range })
+        end, { range = true })
+      '';
     };
   };
 }
