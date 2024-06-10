ecc2836d847790db11641428256bd5fd32d5ae8f
sai <saitheninja@gmail.com>
2024-06-10 20:03:29 +0200

    forgot to rename module option

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -5,17 +5,17 @@
     configZsh.enable = lib.mkEnableOption "Enable customised zsh.";
   };
 
-  config = lib.mkIf config.configSelectedFonts.enable {
+  config = lib.mkIf config.configZsh.enable {
     programs = {
       zsh = {
         enable = true;
-        enableCompletion = true;
-        vteIntegration = true;
 
+        enableCompletion = true;
         shellAliases = {
           cat = "bat";
           ls = "eza --icons=always";
         };
+        vteIntegration = true;
 
         # plugins
         autosuggestions.enable = true;
