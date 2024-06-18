6f5c2eeac5c181e6af95395ba8a6e434c3064129
sai <saitheninja@gmail.com>
2024-06-18 16:26:41 +0200

    nixvim: tweak how line wrapping is shown

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -57,7 +57,7 @@
         shiftwidth = 2; # number of spaces to use for each step of indent
         tabstop = 2; # number of spaces that a tab counts for
         breakindent = true; # wrapped lines will get visually indented
-        showbreak = "->"; # string to put at the start of wrapped lines
+        showbreak = "↳"; # string to put at the start of wrapped lines
 
         # line numbers
         number = true;
@@ -91,6 +91,12 @@
             italic_comments = true;
             transparent = true;
           };
+        };
+      };
+
+      highlightOverride = {
+        NonText = {
+          link = "Conceal"; # override NonText with link to Conceal
         };
       };
 
