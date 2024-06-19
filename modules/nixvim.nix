77290425a30451a2e2171be3dee3dea102b74340
sai <saitheninja@gmail.com>
2024-06-19 21:35:01 +0200

    nixvim: debug conform command
    Range formatting isn't working.
    `opts.range` always returns 0.

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -890,14 +890,27 @@
         ''
           -- from conform docs 
           -- make format command
-          vim.api.nvim_create_user_command("ConformFormat", function(args)
+          vim.api.nvim_create_user_command("ConformFormat", function(opts)
+            -- for k, v in pairs(opts) do
+            --   print(k, v)
+            -- end
+
             local range = nil
-            if args.count ~= -1 then
-              local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+
+            if opts.count ~= -1 then
+              local end_line = vim.api.nvim_buf_get_lines(0, opts.line2 - 1, opts.line2, true)[1]
+
               range = {
-                start = { args.line1, 0 },
-                ["end"] = { args.line2, end_line:len() },
+                start = { opts.line1, 0 },
+                ["end"] = { opts.line2, end_line:len() },
               }
+
+              -- for a, b in pairs(range) do
+              --   print(a)
+              --   for x, y in pairs(b) do
+              --     print(x, y)
+              --   end
+              -- end
             end
             require("conform").format({ async = true, lsp_format = "fallback", range = range })
           end, { range = true })
