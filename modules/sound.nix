7d9d61fec78a80ae8509dd0582362e9e2987f8cc
sai <saitheninja@gmail.com>
2024-07-22 23:00:28 +0200

    lower swappiness

--- modules/sound.nix
+++ modules/sound.nix
@@ -6,6 +6,13 @@
   };
 
   config = lib.mkIf config.configSound.enable {
+    boot.kernel.sysctl = {
+      # lower means use ram more, swap to disk less
+      # helps with audio latency
+      # default = 60
+      "vm.swappiness" = 10;
+    };
+
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
