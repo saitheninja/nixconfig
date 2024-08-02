4b7ff80816574e5aa33360391d836cd1feb84414
sai <saitheninja@gmail.com>
2024-08-02 16:02:29 +0200

    nixvim: move telescope config to its own module file

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     ./lualine.nix
+    ./telescope.nix
   ];
 
   options.configNixvim.enable = lib.mkOption {
@@ -37,11 +38,6 @@
 
         # LSPs
         zls # zig
-
-        # telescope
-        fd # better find
-        fzf # fuzzy find
-        ripgrep # faster grep
       ];
       extraPlugins = with pkgs.vimPlugins; [
         package-info-nvim # npm package info
@@ -539,38 +535,6 @@
             delete_to_trash = true;
           };
         };
-        telescope = {
-          enable = true; # popup fuzzy finder, with previews
-
-          extensions = {
-            fzf-native.enable = true; # fzf sorter written in C
-            media-files.enable = true; # preview media files
-            ui-select.enable = true; # set vim.ui.select to telescope
-            undo.enable = true; # view and search undo tree
-          };
-
-          # provided to the require('telescope').setup function
-          settings = {
-            pickers = {
-              buffers = {
-                mappings = {
-                  # insert mode
-                  i = {
-                    "<C-d>" = {
-                      __raw = "require('telescope.actions').delete_buffer";
-                    };
-                  };
-                  # normal mode
-                  n = {
-                    "dd" = {
-                      __raw = "require('telescope.actions').delete_buffer";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
       };
 
       keymaps = [
@@ -847,81 +811,6 @@
           mode = "n";
           options = {
             desc = "Package Info: change version";
-          };
-        }
-
-        # telescope file pickers
-        {
-          action = "<Cmd>Telescope find_files<CR>";
-          key = "<leader>ff";
-          mode = "n";
-          options = {
-            desc = "Telescope: find files";
-          };
-        }
-        {
-          action = "<Cmd>Telescope live_grep<CR>";
-          key = "<leader>fg";
-          mode = "n";
-          options = {
-            desc = "Telescope: live grep";
-          };
-        }
-        # telescope vim pickers
-        {
-          action = "<Cmd>Telescope autocommands<CR>";
-          key = "<leader>fa";
-          mode = "n";
-          options = {
-            desc = "Telescope: autocommands";
-          };
-        }
-        {
-          action = "<Cmd>Telescope buffers<CR>";
-          key = "<leader>fb";
-          mode = "n";
-          options = {
-            desc = "Telescope: buffers";
-          };
-        }
-        {
-          action = "<Cmd>Telescope commands<CR>";
-          key = "<leader>fc";
-          mode = "n";
-          options = {
-            desc = "Telescope: plugin/user commands";
-          };
-        }
-        {
-          action = "<Cmd>Telescope help_tags<CR>";
-          key = "<leader>fh";
-          mode = "n";
-          options = {
-            desc = "Telescope: help tags";
-          };
-        }
-        {
-          action = "<Cmd>Telescope marks<CR>";
-          key = "<leader>fm";
-          mode = "n";
-          options = {
-            desc = "Telescope: marks";
-          };
-        }
-        {
-          action = "<Cmd>Telescope oldfiles<CR>";
-          key = "<leader>fo";
-          mode = "n";
-          options = {
-            desc = "Telescope: previously opened files";
-          };
-        }
-        {
-          action = "<Cmd>Telescope vim_options<CR>";
-          key = "<leader>fv";
-          mode = "n";
-          options = {
-            desc = "Telescope: vim options";
           };
         }
 
