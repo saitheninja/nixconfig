d0851bbb4f6a0e6a816fd42cc4e011df029f5e97
sai <saitheninja@gmail.com>
2024-08-20 15:50:25 +0200

    clean up

--- modules/kde-plasma-6.nix
+++ modules/kde-plasma-6.nix
@@ -65,9 +65,10 @@ in
 
     xdg.portal = {
       enable = true;
-      # Sets environment variable `NIXOS_XDG_OPEN_USE_PORTAL` to `1`.
-      # This will make `xdg-open` use the portal to open programs, which resolves bugs 
-      # involving programs opening inside FHS envs or with unexpected env vars set from wrappers.
+
+      # Sets environment variable `NIXOS_XDG_OPEN_USE_PORTAL=1`
+      # This will make `xdg-open` use the portal to open programs
+      # which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers
       xdgOpenUsePortal = true;
     };
   };
