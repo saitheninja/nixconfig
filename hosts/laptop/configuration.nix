26044a18c291920834f6278f553a682bc70e4a29
sai <saitheninja@gmail.com>
2024-11-09 12:47:51 +0200

    Change order of home row mods.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -247,15 +247,15 @@
             ;; switch to no-mods layer if pressing a key on the same side
             ;; switch back to default-layer after 20ms timeout
 
-            a (tap-hold-release-keys $mods-timeout $mods-timeout (multi a @same-side) lmet $left-hand-keys)
-            s (tap-hold-release-keys $mods-timeout $mods-timeout (multi s @same-side) lalt $left-hand-keys)
+            a (tap-hold-release-keys $mods-timeout $mods-timeout (multi a @same-side) lalt $left-hand-keys)
+            s (tap-hold-release-keys $mods-timeout $mods-timeout (multi s @same-side) lmet $left-hand-keys)
             d (tap-hold-release-keys $mods-timeout $mods-timeout (multi d @same-side) lctl $left-hand-keys)
             f (tap-hold-release-keys $mods-timeout $mods-timeout (multi f @same-side) lsft $left-hand-keys)
 
             j (tap-hold-release-keys $mods-timeout $mods-timeout (multi j @same-side) rsft $right-hand-keys)
             k (tap-hold-release-keys $mods-timeout $mods-timeout (multi k @same-side) rctl $right-hand-keys)
-            l (tap-hold-release-keys $mods-timeout $mods-timeout (multi l @same-side) ralt $right-hand-keys)
-            scln (tap-hold-release-keys $mods-timeout $mods-timeout (multi scln @same-side) rmet $right-hand-keys)
+            l (tap-hold-release-keys $mods-timeout $mods-timeout (multi l @same-side) rmet $right-hand-keys)
+            scln (tap-hold-release-keys $mods-timeout $mods-timeout (multi scln @same-side) ralt $right-hand-keys)
           )
         '';
 
