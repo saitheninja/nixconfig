8f327377589b0e1eb92ffb7e81dab33b96ec4e49
sai <saitheninja@gmail.com>
2025-02-16 21:48:32 +0200

    Option renamed

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -90,7 +90,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
-  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
+  hardware.graphics.enable32Bit = true; # Enables support for 32bit libs that steam uses
 
   #environment.variables.EDITOR = "vim";
 
